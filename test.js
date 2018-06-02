520b09fb512ee0fc19c26805243dd985a4f8875b
Jeroen van der Heijden <jeroen@transceptor.technology>
2018-06-02 13:10:39 +0200

    Adding more tests

--- test.js
+++ test.js
@@ -28,7 +28,33 @@ var DummyReact = {
     Component: DummyComponent
 };
 
-describe('defineReact',  function () {
+var getApp = function() {
+    var App = {};
+    var MyActions = Vlow.createActions(['add', 'pop']);
+    var MyStore = function() {
+        Vlow.Store.call(this, MyActions);
+        this.state = {
+            items: []
+        };
+    };
+
+    MyStore.prototype.onAdd = function(item) {
+        this.setState({
+            items: this.state.items.slice().push(item)
+        });
+    };
+
+    MyStore.prototype.onPop = function() {
+        if (this.state.items.length) {
+            this.setState({
+                items: this.state.items.slice(0, -1)
+            });
+        }
+    };
+    App.myStore
+};
+
+describe('Test Vlow.defineReact', function () {
 
     it('Define invalid arguments as React', function () {
         assert.throws(function() { Vlow.defineReact(); });
@@ -38,6 +64,33 @@ describe('defineReact',  function () {
 
     it('Define DummyReact as React', function () {
         assert.doesNotThrow(function() { Vlow.defineReact(DummyReact); });
+    });
+
+});
+
+describe('Test Vlow.createActions', function () {
+
+    it('Create duplicate action', function () {
+        assert.throws(function() { Vlow.createActions(['duplicate', 'duplicate']); });
+    });
+
+    it('Create empty action', function () {
+        assert.throws(function() { Vlow.createActions(['']); });
+    });
+
+    it('Create non-string action', function () {
+        assert.throws(function() { Vlow.createActions([null]); });
+    });
+
+    it('Create action starting with an underscore', function () {
+        assert.throws(function() { Vlow.createActions(['_doThis']); });
+    });
+
+    it('Create valid actions', function () {
+        let result;
+        assert.doesNotThrow(() => { result = Vlow.createActions(['fetch', 'update']); });
+        assert.equal(typeof result.fetch, 'function');
+        assert.equal(typeof result.update, 'function');
     });
 
 });
